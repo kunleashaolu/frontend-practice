dcf20ee9ceba63c206d8f16b9a695bb163312627
Adekunle Ashaolu <dekxnle@gmail.com>
2023-06-03 22:21:17 +0100

    chore: wrap up nav section

--- ableton/index.js
+++ ableton/index.js
@@ -5,6 +5,7 @@ const navTriggerBtn = document.getElementById('nav-trigger-btn')
 const navWrapper = document.getElementById('main-nav-wrapper')
 const navContent = document.getElementById('main-nav-content')
 const navLinkList = document.getElementsByClassName('main-nav__link-list')
+const primaryNavSection = document.getElementById('primary-nav-section')
 
 moreButton.addEventListener('click', () => {
   if (moreSection.ariaHidden === 'true') {
@@ -20,12 +21,15 @@ moreButton.addEventListener('click', () => {
 
 navTriggerBtn.addEventListener('click', () => {
   navWrapper.classList.replace('flex-x', 'flex-y')
-  navContent.style.display = 'block'
-  moreSection.style.display = 'flex'
+  navContent.classList.remove('is-collapsed')
+  moreSection.classList.remove('is-collapsed')
   moreSection.ariaHidden = 'false'
+
+  primaryNavSection.classList.add('is-expanded')
 
   for (let i = 0; i < navLinkList.length; i++) {
     navLinkList[i].classList.add('flex-y')
     navLinkList[i].classList.replace('align-center', 'align-start')
+    navLinkList[i].classList.add('gap-0')
   }
 })
